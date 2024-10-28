f61d9121b52c1f29b5f6e4f54119eb63f7778395
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-10-28 14:51:20 +0900

    swiftSyntaxBasic

---
+++ Swift-Syntax-Basic.playground/Contents.swift
@@ -0,0 +1,128 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hello, World!")
+print(greeting)
+
+/*
+ my name is brody //  기본 문장
+ myNameIsBrody // Camel Case
+ 
+ Hello World // 기본 문장
+ HelloWrold // Camel Case
+ 
+ sheLovesCoding
+ 
+ weAreLearningSwift
+ 
+ theSkyIsBlue
+ 
+ iLikeCoffee
+ 
+ myFavoriteColorIsRed
+ 
+ thisIsATest
+ 
+ programmingIsFun
+ 
+ appleMakesGreatProducts
+ 
+ teamworkIsImportant
+ 
+ theCatIsSleeping
+ 
+ iWantToTravel
+ 
+ weAreDevelopers
+ 
+ learningSwiftIsEasy
+ 
+ myComputerIsNew
+ 
+ */
+
+
+let name: String
+name = "Brody"
+print(name)
+
+let computer = 150
+print(computer)
+
+
+var age: Int
+age = 20
+print(age)
+age = 25
+print(age)
+
+var name2: String
+name2 = "JeongMu"
+print(name2)
+name2 = "YangJeongMu"
+print(name2)
+
+var name3: String
+name3 = "개"
+print(name3)
+
+var name4: String
+name4 = "타우린"
+print(name4)
+
+var name5: String
+name5 = "울트라 졸림"
+print(name5)
+
+var name6: String
+name6 = "교양과목 들어야함"
+print(name6)
+
+var name7: String
+name7 = "이거 20개 채운사람 없겠지?"
+print(name7)
+
+var age2: Int
+age2 = 33
+print(age2)
+
+var age3: Int
+age3 = 35
+print(age3)
+
+let name10: String
+name10 = "고달픈 인생"
+print(name10)
+
+let age10: Int
+age10 = 35
+print(age10)
+
+let name11: String
+name11 = "인생"
+print(name11)
+
+//2. 선언과 동시에 값을 할당하는 방법
+
+var name100: String = "이름"
+print(name100)
+name100 = "이름을 바꿀수 있어요"
+print(name100)
+
+let age20: Int = 20
+print(age)
+
+//3. 타입을 사용하지 않고 바로 값을 할당하기.
+
+var name101 = "101"
+print(name101)
+
+let age101 = 101
+print(age101)
+
+// 4
+var myName = "jeongmu"
+var MyHobby = "game"
+print(myName, MyHobby)
+print("\(myName)'s hobby is \(MyHobby).")
