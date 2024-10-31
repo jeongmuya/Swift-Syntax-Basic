b67af5109659e086918ec1b1553ddf2eb9661f14
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-11-01 02:23:10 +0900

    1-8 반복문

---
+++ 1-8 반복문.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+
+
+
+//for i in 1...10 {
+//    print(i)
+//    guard i < 8 else {break}
+//}
+
+//let fruits = ["사과", "바나나", "포도"]
+//
+//for i in fruits {
+//    print(i)
+//}
+
+
+//for i in 1...20 {
+//    if i % 2 == 0 {
+//        print(i)
+//    }
+//}
+
+
+//var countdown = 11
+//
+//while countdown <= 11 {
+//    countdown -= 1
+//    print(countdown)
+//    
+//    if countdown == 1 {
+//       break
+//    }
+//}
+
+
+var OneToFive = 0
+var sumOfOneToFive = 0
+
+while OneToFive < 5 {
+    OneToFive += 1
+    sumOfOneToFive += OneToFive
+    print(sumOfOneToFive)
+    
+}
+
+
