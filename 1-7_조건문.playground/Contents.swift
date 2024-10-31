2783f454fe3440c63e2416bdae0c97ae8242d3aa
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-10-31 17:30:01 +0900

    1-7 조건문

---
+++ 1-7_조건문.playground/Contents.swift
@@ -0,0 +1,105 @@
+import UIKit
+
+//if 10 > 20 {
+//    print("10은 20보다 큽니다!")
+//} else {
+//    print("10은 20보다 작습니다!")
+//}
+//
+//
+//var age = 20
+//if age >= 20 {
+//    print("20살이 넘은 성인입니다.")
+//} else if age >= 17 {
+//    print("20살 미안이고 17살 이상인 고등학생입니다.")
+//} else if age >= 14 {
+//    print(" 17살 미만이고 14살 이상인 중학생입니다.")
+//} else {
+//    print("어린이 입니다!")
+//}
+//
+//let score = 80
+//
+//switch score {
+//    case 90...100:
+//        print("A 등급 입니다!")
+//    case 80..<90:
+//        print("B 등급 입니다.")
+//    case 70..<80:
+//        print("C 등급 입니다.")
+//    default:
+//        print("D 등급 입니다.")
+//}
+
+//var age = 50
+//
+//if age >= 20 {
+//    print("성인입니다.")
+//} else if 20 >= 8 {
+//    print("학생입니다.")
+//} else if age < 8 {
+//    print("어린이입니다.")
+//}
+//
+//var username = "admin"
+//
+//if username == "admin" {
+//    print("관리자입니다.")
+//} else if username == "guest" {
+//    print("손님입니다")
+//} else {
+//    print("일반 사용자입니다")
+//}
+
+
+//var number = 17
+//
+//if number == 17%2 {
+//    print("짝수입니다.")
+//}else{
+//    print("홀수입니다")
+//}
+
+//var day = "토요일"
+//
+//switch day {
+//case "월요일":
+//    print("출근합니다.")
+//case "토요일":
+//    print("휴식합니다.")
+//case "일요일":
+//    print("휴식합니다.")
+//default:
+//    print("일상적인 일과를 수행합니다.")
+//}
+
+
+//var fruit = "사과"
+//
+//switch fruit {
+//case "사과":
+//    print("빨간색")
+//case "바나나":
+//    print("노란색")
+//case "포도":
+//    print("보라색")
+//default:
+//    print("알 수 없는 색상")
+//}
+
+//var season = "휴가"
+//
+//switch season {
+//case "봄":
+//    print("꽃구경을 갑니다")
+//case "여름":
+//    print("해수욕을 갑니다")
+//case "가을":
+//    print("단풍을 구경합니다")
+//case "겨울":
+//    print("스키를 탑니다")
+//default:
+//    print("집에서 쉽니다")
+//}
+
+
